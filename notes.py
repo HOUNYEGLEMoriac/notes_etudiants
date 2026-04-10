3c37ee2c161ed24727743e14199d4719fafdd74c
HOUNYEGLEMoriac <hounyeglesedjromoriacchabroleb@gmail.com>
2026-04-10 10:25:15 +0100

    premiere_modification

--- notes.py
+++ notes.py
@@ -4,3 +4,12 @@ etudiants=[]
 def ajouter_etudiants(nom,note):
     etudiants.append({"nom":nom,"note":note})
     print(f"Etudiants {nom}ajouté avec la note {note}.")
+    
+def calculer_moyenne():
+    if not etudiants:
+    print("Aucun étudiant enrégistré.")
+    return 0
+   total=sum(e['note']for e in etudiants)
+   moyenne=total/|en(etudiants)
+   print(f"Moyenne de le classe:{moyenne:.2f}")
+   return moyenne
